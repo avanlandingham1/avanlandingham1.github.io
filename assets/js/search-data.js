aa291223091b1196a4b95a909565fdc0a5a53889
avanlandingham1 <130082214+avanlandingham1@users.noreply.github.com>
2025-12-02 15:22:03 +0000

    Deploying to gh-pages from @ avanlandingham1/avanlandingham1.github.io@a1cfdd1e82e350d4caaa6835ae4f3cb3e7e74533 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -83,7 +83,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61.%76%61%6E%6C%61%6E%64%69%6E%67%68%61%6D%30%31@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-linkedin',
